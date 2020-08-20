4c3f194150bce6d85e220fc7db0020bee767a2e3
Jairo Doni <jairo.doni97@gmail.com>
2020-08-20 17:25:06 -0300

    configuracao dotenv

--- project/server/src/index.js
+++ project/server/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+require('dotenv/config');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0.4yhmh.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+mongoose.connect(process.env.SERVER_MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex:true,
